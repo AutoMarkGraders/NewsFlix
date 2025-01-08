10b287a8755fc6906acdf76526dd5d05565533cf
Alan Joseph <notalanjoseph@gmail.com>
2025-01-08 14:51:12 +0530

    new files, auth

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,9 +1,36 @@
 # Pydantic models for the request and response bodies of the API
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, constr
 from pydantic.types import conint
 from datetime import datetime, date
-from typing import Optional
+from typing import Optional, List
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: constr(min_length=4)
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True # to convert to a pydantic model since this class is used as a response
+
+# use Outh2PasswordRequestForm instead
+# class UserLogin(BaseModel):
+#     email: EmailStr
+#     password: constr(min_length=4)
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[int] = None
+
+
 
 
 class TextInput(BaseModel):
