94f6dcadb8aefc79e056df8bf197dc0960aa8555
Alan Joseph <notalanjoseph@gmail.com>
2025-01-19 23:16:13 +0530

    reel gen algorithm v2.0

--- backend/app/generator.py
+++ backend/app/generator.py
@@ -1,7 +1,6 @@
 import moviepy.editor as mp
 from moviepy.config import change_settings
 change_settings({"IMAGEMAGICK_BINARY": r"C:\Program Files\ImageMagick-7.1.1-Q16-HDRI\magick.exe"}) # path to ImageMagick executable
-from moviepy.editor import *
 from moviepy.video.VideoClip import TextClip
 
 from math import ceil
@@ -13,10 +12,18 @@ from gtts import gTTS
 
 def generate(summary, category):
 
-    n = summary.count(' ') + 1 # no.of words
-    print(f'Number of words: {n}')
+# gtts to get reel.mp3
+    tts = gTTS(text=summary, lang='en')
+    tts.save("reel.mp3")
 
-    # fetch background images
+# set duration as 85% of reel.mp3 duration
+    narration = mp.AudioFileClip("reel.mp3")
+    duration = narration.duration * 0.85
+    narration = narration.fx(mp.vfx.speedx, final_duration=duration)
+    print(f'Duration = {duration}')
+
+# fetch background images
+    n = ceil(duration/10)
     load_dotenv()
     API_KEY = os.getenv('PEXELS_KEY')
     url = 'https://api.pexels.com/v1/search'
@@ -25,7 +32,7 @@ def generate(summary, category):
     }
     params = {
         'query': f'{category}',  # search term
-        'per_page': ceil(n/25),  # Number of results 
+        'per_page': n,  # Number of images 
         'orientation': 'landscape',
     }
     response = requests.get(url, headers=headers, params=params).json()
@@ -33,56 +40,44 @@ def generate(summary, category):
         image_url = photo['src']['original']
         img_data = requests.get(image_url).content
         with open(f'{i}.jpg', 'wb') as handler: # saved in CWD (backend/)
-            handler.write(img_data)    
+            handler.write(img_data)
+    print(f'no.of images = {n}')
 
-    # Video settings
+# set background images
     width, height = 1080, 1920  # aspect ratio 9:16
-    duration = n/25 * 10  # in seconds
-    print(f'Duration= {duration}')
+    video = mp.ColorClip(size=(width, height), color=(0, 0, 0), duration=0)
+    
+    backgrounds = []
+    for i in range(1, n + 1):
+        bg = mp.ImageClip(f"{i}.jpg").set_duration(10)
+        # new width after resizing to maintain aspect ratio
+        image_width, image_height = bg.size
+        aspect_ratio = image_width / image_height
+        new_width = int(aspect_ratio * height)
+        bg = bg.resize(height=height)
+        # animation to move the image fully across the screen
+        bg = bg.set_position(lambda t: ((width - new_width) * (t / 10), 0))
 
-    background = ImageClip('1.jpg').set_duration(duration)
+        backgrounds.append(bg)
 
-    # Calculate the new width after resizing to maintain the image's aspect ratio
-    image_width, image_height = background.size
-    aspect_ratio = image_width / image_height
-    new_width = int(aspect_ratio * height)  # Adjust width according to the height to fit the mobile screen height
-    # Resize the image to match the screen height while keeping the aspect ratio
-    background = background.resize(height=height)
-    # Adjust the animation to move the image fully across the screen
-    background = background.set_position(lambda t: (int(min(0, (width - new_width) * (1 - t / duration))), 0))
+    # # fast but no animation
+    # background = mp.concatenate_videoclips(backgrounds)
+    # video = mp.CompositeVideoClip([background], size=(width, height))
 
-    # SUBTITLE SETTINGS
-    #input_text = "This is a long string that will scroll through the screen like a subtitle."
-    font_size = 150
-    font_color = 'yellow'
+    # slow but with animation
+    for bg in backgrounds:
+        video = mp.CompositeVideoClip([video, bg.set_start(video.duration)], size=(width, height))
 
-    # Create the text clip (the width can be larger than the screen to allow movement)
-    text_clip = mp.TextClip(summary, fontsize=font_size, color=font_color, font='Arial-Bold', size=(None, None), stroke_color="black", stroke_width=5)
-
-    # Length the text should move
-    text_width = text_clip.size[0]
-
-    # text scroll from right to left
-    text_clip = text_clip.set_position(lambda t: (width - int((text_width + width) * t / duration), height - 700)).set_duration(duration)
-
-    # Composite the text on the background
-    video = mp.CompositeVideoClip([background, text_clip], size=(width, height))
-
-
-    # narration audio
-    tts = gTTS(text=summary, lang='en')
-    tts.save("reel.mp3")
-    narration = mp.AudioFileClip("reel.mp3")
-    narration = narration.fx(mp.vfx.speedx, final_duration=duration)
+# Set the audio to the video
     video = video.set_audio(narration)
 
-    # Write the final video file
-    video.write_videofile("reel.mp4", fps=24) # saved in CWD (backend/)
+    # transcript
+    # add captions
 
-# better algo
-# gtts to get reel.mp3
-# set duration as 80% of reel.mp3 duration
-# make video with multiple images
-# add audio to video
-# transcript
-# add captions
+    video.write_videofile("reel.mp4", fps=24)        
+
+
+if __name__ == '__main__':
+    summary = "Slovenian handball team makes it to Paris Olympics semifinal Lille, 8 August - Slovenia defeated Norway 33:28 in the Olympic men's handball tournament in Lille late on Wednesday to advance to the semifinal where they will face Denmark on Friday evening. This is the best result the team has so far achieved at the Olympic Games and one of the best performances in the history of Slovenia's team sports squads"
+    category = "handball"
+    generate(summary, category)
